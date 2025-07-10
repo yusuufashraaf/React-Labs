f274c5ca935c14bd89f607121ecd7770007fd5dd
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-07-10 14:16:53 +0300

    Add Lab 3

---
+++ Lab 3/Movies/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MoviesAPI from './assets/Movie/MoviesAPI'
+import Parent from './assets/Parent/Parent'
+import Search from './assets/Search/Search'
+
+function App() {
+
+  return (
+    <>
+    <Parent />
+    </>
+  )
+}
+
+export default App
